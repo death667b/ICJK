e3a923398356f65aa118552aa7d7094d960031a9
terminal29 <20899505+terminal29@users.noreply.github.com>
2018-09-01 16:44:38 +1000

    [S15] Adding search bar Added a functional search bar to the top of the page Changed colour scheme Removed link to staff login page Reorganised view generation functions

--- ICJK/Home/views.py
+++ ICJK/Home/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci justo, facilisis non pulvinar aliquam, ullamcorper vel purus."
-# Create your views here.
-def index(request):
-    return redirect("/personal")
 
-def personal(request):
-    return render(request, "Home/index.html", {
-        "appname": "ICJK Car Rentals",
-        "viewtype":"personal",
-        "carlist":[
+dummy_carlist = [
             {
                 "name": "Suzuki Swift",
                 "desc": lorem,
@@ -30,14 +24,6 @@ def personal(request):
                 "desc": lorem,
                 "link": "Hyundai_Elantra"
             },
-        ]
-    })
-
-def commercial(request):
-    return render(request, "Home/index.html", {
-        "appname": "ICJK Car Rentals",
-        "viewtype": "commercial",
-        "carlist": [
             {
                 "name": "Toyota Landcruiser Workmate",
                 "desc": lorem,
@@ -53,5 +39,33 @@ def commercial(request):
                 "desc": lorem,
                 "link": "Toyota_Hiace"
             }
-        ]
+]
+
+
+# Create your views here.
+def index(request):
+    return redirect("/personal")
+
+def search_view(request, viewtype):
+
+    query = request.GET.get('query', None)
+
+    query_result = []
+    if query is not None:
+        # Simple dummy search until actual search is implemented
+        result = [car for car in dummy_carlist if query in car["name"]]
+
+    return render(request, "Home/index.html", {
+        "appname": "ICJK Car Rentals",
+        "query": query_result,
+        "viewtype":viewtype,
+        "carlist":result
     })
+
+def personal(request):
+    return search_view(request, "personal")
+
+def commercial(request):
+    return search_view(request, "commercial")
+
+
