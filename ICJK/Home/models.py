cc43a7f16d5cf238e51b2a67495ab284c75629a3
death667b <idaniel@spinnys.com>
2018-09-02 12:21:11 +1000

    Added the car model

--- ICJK/Home/models.py
+++ ICJK/Home/models.py
@@ -21,3 +21,22 @@ class Customer(models.Model):
     birthday = models.DateField()
     occupation = models.CharField(max_length=100)
     gender = models.CharField(max_length=20)
+
+class Car(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+    )
+    make_name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    series = models.CharField(max_length=100)
+    series_year = models.IntegerField()
+    price_new = models.IntegerField()
+    engine_size = models.FloatField()
+    fuel_system = models.CharField(max_length=100)
+    tank_capacity = models.FloatField()
+    power =  models.IntegerField()
+    seating_capacity = models.IntegerField()
+    standard_transmission = models.CharField(max_length=100)
+    body_type = models.CharField(max_length=100)
+    drive = models.CharField(max_length=100)
+    wheelbase = models.IntegerField()
