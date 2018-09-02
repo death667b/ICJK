a7cc641b84d9e3b7bffe258d8062ce57e8738617
death667b <idaniel@spinnys.com>
2018-09-02 22:45:00 +1000

    Registered model to the admin console

--- ICJK/Home/admin.py
+++ ICJK/Home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Store, Customer, Car, Order
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Customer)
+admin.site.register(Car)
+admin.site.register(Order)
