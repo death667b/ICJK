75826b6b7b28c9a0cba05f683197aaaa71c14a72
death667b <idaniel@spinnys.com>
2018-09-02 12:20:42 +1000

    Added the customer model

--- ICJK/Home/models.py
+++ ICJK/Home/models.py
@@ -10,3 +10,14 @@ class Store(models.Model):
     phone = models.IntegerField()
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
+
+class Customer(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+    )
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    birthday = models.DateField()
+    occupation = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
