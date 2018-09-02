e759fe2e5533746d16b802d3368eb9fc5c74d024
death667b <idaniel@spinnys.com>
2018-09-02 12:20:28 +1000

    Added the store model

--- ICJK/Home/models.py
+++ ICJK/Home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+    )
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
