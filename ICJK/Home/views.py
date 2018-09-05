c2b33deff36f7dc32274b30580ef30f9248f487f
terminal29 <20899505+terminal29@users.noreply.github.com>
2018-09-05 16:29:45 +1000

    [S15] Adding db search functionality Added ability to search for cars from the database using text

--- ICJK/Home/views.py
+++ ICJK/Home/views.py
@@ -1,46 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci justo, facilisis non pulvinar aliquam, ullamcorper vel purus."
-
-dummy_carlist = [
-            {
-                "name": "Suzuki Swift",
-                "desc": lorem,
-                "link": "Suzuki_Swift"
-            },
-            {
-                "name": "Toyota Yaris",
-                "desc": lorem,
-                "link": "Toyota_Yaris"
-            },
-            {
-                "name": "Toyota Corolla",
-                "desc": lorem,
-                "link": "Toyota_Corolla"
-            },
-            {
-                "name": "Hyundai Elantra",
-                "desc": lorem,
-                "link": "Hyundai_Elantra"
-            },
-            {
-                "name": "Toyota Landcruiser Workmate",
-                "desc": lorem,
-                "link": "Toyota_Landcruiser_Workmate"
-            },
-            {
-                "name": "Mercedes-Benz Sprinter",
-                "desc": lorem,
-                "link": "Mercedes-Benz_Sprinter"
-            },
-            {
-                "name": "Toyota Hiace",
-                "desc": lorem,
-                "link": "Toyota_Hiace"
-            }
-]
-
+from .models import Car
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -51,15 +12,29 @@ def search_view(request, viewtype):
     query = request.GET.get('query', None)
 
     query_result = []
-    if query is not None:
-        # Simple dummy search until actual search is implemented
-        query_result = [car for car in dummy_carlist if query in car["name"]]
+    if query is not None and query is not '':
+        query_words = query.split()
+        print(query_words)
+        db_query = Q()
+        for word in query_words:
+            db_query &= Q(Q(make_name__contains=word) | Q(model__contains=word))
+        #db_query.add(Q(model__in=query_words), Q.OR)
+
+        query_set = Car.objects.filter(db_query).order_by('make_name', 'model', 'series')
+        query_result = [
+            {"name": ("%s %s %s"%(car.make_name.title(), car.model.title(), car.series.title())),
+             "desc": "The %s %s %s made in %i is a %s %s with %i seats and a %i horsepower %iL engine." %
+                     (car.make_name.title(), car.model.title(), car.series, car.series_year, car.body_type.lower(), car.drive.lower()
+                      , car.seating_capacity, car.power, car.engine_size),
+             "link": car.id}
+            for car in query_set
+        ]
 
     return render(request, "Home/index.html", {
         "appname": "ICJK Car Rentals",
         "query": query,
-        "viewtype":viewtype,
-        "carlist":query_result
+        "viewtype": viewtype,
+        "carlist": query_result
     })
 
 def personal(request):
