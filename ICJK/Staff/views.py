654abb10dd282ed46a5dab68c7d401c2d67a9e26
terminal29 <20899505+terminal29@users.noreply.github.com>
2018-10-01 13:20:23 +1000

    Staff login page
    Created staff login page with basic form and buttons

--- ICJK/Staff/views.py
+++ ICJK/Staff/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.contrib.sites.shortcuts import get_current_site
 # Create your views here.
 def index(request):
-    return JsonResponse({'foo':'bar'})
+    return render(request, "Staff/index.html", {
+        "applink": "http://" + get_current_site(request).domain + "/",
+        "appname": "ICJK Car Rentals"
+    })
