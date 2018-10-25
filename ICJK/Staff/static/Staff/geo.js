cee8c9ac578ff9783adabce841c8a801652572ea
ngtk <khai5121997@gmail.com>
2018-10-25 16:52:23 +1000

    Store location marker add on map

--- ICJK/Staff/static/Staff/geo.js
+++ ICJK/Staff/static/Staff/geo.js
@@ -4,5 +4,27 @@ function initMap() {
     center: {lat: -26, lng: 134.644},
     zoom: 5
     });
-
 }
+
+geocode('brisbane');    
+
+function geocode(location){
+var location ;
+axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params:{
+        address:location,
+        key:'AIzaSyChkL1-v3Cgf3zc1BfNFucXnovUyZsRiBQ'
+     }
+    })
+    .then(function(response){
+    // Log full response
+    console.log(response.data.results[0]);  
+    // Geometry
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    console.log(lat,lng);
+    })
+    .catch(function(error){
+    console.log(error);
+    });
+}  
